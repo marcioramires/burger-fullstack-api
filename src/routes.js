df34ccbe4d2ad2b44481ddbe7d73f4de818059ab
marcioramires <marcioramiresgt@gmail.com>
2022-12-18 19:29:28 -0300

    login validation updated

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 
+import SessionController from './app/controllers/SessionController.js'
 import UserController from './app/controllers/UserController.js'
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
+
+routes.post('/sessions', SessionController.store)
 
 export default routes
