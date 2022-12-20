1ba06a8baaff733a0b3c07082f62f6c652405320
marcioramires <marcioramiresgt@gmail.com>
2022-12-19 21:06:45 -0300

    Products controller and models created

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize'
 
+import Product from '../app/models/Product.js'
 import User from '../app/models/User.js'
 import configDatabase from '../config/database'
 
-const models = [User]
+const models = [User, Product]
 class Database {
     constructor() {
         this.init()
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,20 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer.js'
 
+import ProductController from './app/controllers/ProductController.js'
 import SessionController from './app/controllers/SessionController.js'
 import UserController from './app/controllers/UserController.js'
+
+const upload = multer(multerConfig)
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
+
+routes.post('/products', upload.single('file'), ProductController.store)
+routes.get('/products', ProductController.index)
 
 export default routes
